d4d907614abf4f79c025018fe057f3d6f3667558
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2015-11-14 14:28:49 +0100

    Read from file; test

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "parser.h"
 #include "ast.h"
@@ -8,8 +9,25 @@
 
 int main(int argc, char** argv)
 {
-	printf("Input: %s\n", argv[1]);
-	tokenizer_t* tokenizer = create_tokenizer(argv[1]);
+	if (argc < 2) {
+		puts("Usage: dami source_file");
+		return -1;
+	}
+	FILE* input_file = fopen (argv[1], "rb");
+	if (!input_file) {
+		puts("Cannot open imput file");
+		return -1;
+	}
+	char* buffer = NULL;
+	long length;
+	fseek (input_file, 0, SEEK_END);
+	length = ftell (input_file);
+	fseek (input_file, 0, SEEK_SET);
+	buffer = malloc(length + 1);
+	fread (buffer, 1, length, input_file);
+	printf("Input: %s\n", buffer);
+	buffer[length] = 0;
+	tokenizer_t* tokenizer = create_tokenizer(buffer);
 	parser_t* parser = create_parser(tokenizer);
 	ast_node_t* root = parse(parser);
 	puts("***********");
@@ -24,5 +42,6 @@ int main(int argc, char** argv)
 	puts("***********");
 	interpreter_t* interpreter = create_interpreter();
 	process_ast_node(interpreter, root);
+	fclose(input_file);
 	return 0;
 }
